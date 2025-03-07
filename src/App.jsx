c5b6848b59f5809efae067bcdcbc75e6f0a74f69
Leekwanyong <dlrkdid12@gmail.com>
2025-03-07 19:26:14 +0900

    feat: notfound 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router';
 import Home from './pages/home/Home.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
