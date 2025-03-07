c34e9ab025ffdd5c5b67ad632b7d38f61fd98de9
Leekwanyong <dlrkdid12@gmail.com>
2025-03-07 19:43:58 +0900

    feat: color 정리

---
+++ src/styles/tailwind.colors.js
@@ -0,0 +1,10 @@
+module.exports = {
+  primary: '#E50914',
+  black: '#141414',
+  darkGray: '#181818',
+  gray: '#303030',
+  lightGray: '#808080',
+  white: '#FFFFFF',
+  blue: '#0071EB',
+  overlay: 'rgba(0, 0, 0, 0.6)',
+};
