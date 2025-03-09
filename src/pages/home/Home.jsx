42107eb655ce2943a0e78fcf747cea76853a9630
Leekwanyong <dlrkdid12@gmail.com>
2025-03-09 23:02:35 +0900

    feat: 쿼리스로 병렬 요청하는 것을 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,36 @@
+import { useQueries } from '@tanstack/react-query';
+import { getPopularMovies } from '../../api/getMovieApi.js';
+import Slider from '../../components/common/SwiperSlider/Slider.jsx';
 import Layout from '../../layout/Layout.jsx';
 
+const CATEGORIES = ['now_playing', 'popular', 'top_rated', 'upcoming'];
+const LENGTH = [0, 1, 2, 3];
 function Home() {
-  return <Layout>home</Layout>;
+  // Use Hook 뺼 생각을 해봐야 겠음
+  const userQueries = useQueries({
+    queries: CATEGORIES.map((item) => {
+      return {
+        queryKey: ['slider', item],
+        queryFn: async () => {
+          return await getPopularMovies(item);
+        },
+      };
+    }),
+  });
+
+  const queriesData = userQueries.map((item) => item.data?.results?.map((v) => v));
+  return (
+    <Layout>
+      <div>
+        {CATEGORIES.map((category, index) => (
+          <div key={category} className=" flex items-start flex-col justify-between">
+            <p className="text-xl text-white font-semibold mt-4 ">{category}</p>
+            <Slider items={queriesData[index]} />
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
 export default Home;
