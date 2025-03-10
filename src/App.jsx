66337988c575d22e414bc59ba5c01baeb24511f6
Leekwanyong <dlrkdid12@gmail.com>
2025-03-10 17:01:18 +0900

    feat: movies 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router';
 import Layout from './layout/Layout.jsx';
 import Home from './pages/home/Home.jsx';
+import Movies from './pages/movies/Movies.jsx';
 import NotFound from './pages/NotFound.jsx';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
