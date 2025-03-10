364b620c5f215cc75b7dc17eb60d233c0b22dfa2
Leekwanyong <dlrkdid12@gmail.com>
2025-03-10 09:24:26 +0900

    feat: link 태그 추가

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router';
+
 function NotFound() {
   return (
     <div className="bg-black text-white h-screen text-center">
@@ -7,9 +9,11 @@ function NotFound() {
           죄송합니다. 해당 페이지를 찾을 수 없습니다. 홈페이지로 이동해 <br />
           다양한 콘텍츠를 만나보세요.
         </p>
-        <button className="border border-white py-2 px-8 rounded-md bg-white text-black font-semibold hover:bg-gray-200">
-          Netflix 홈
-        </button>
+        <Link to="/">
+          <button className="border border-white py-2 px-8 rounded-md bg-white text-black font-semibold hover:bg-gray-200">
+            Netflix 홈
+          </button>
+        </Link>
       </div>
       <div className="flex  justify-center items-center ">
         <div className="h-[0.1rem] bg-red-500 w-[4%] rotate-90"></div>
