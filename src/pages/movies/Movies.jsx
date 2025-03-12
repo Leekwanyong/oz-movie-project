42b4001e6cae3719ca9e880b0166d48eea786909
Leekwanyong <dlrkdid12@gmail.com>
2025-03-12 14:28:42 +0900

    feat: 검색 결과 데이터 추가

--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -1,22 +1,34 @@
-import { useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router';
+import { getSearchMovies } from '../../api/getMovieApi.js';
 import InfiniteScrollMovieCard from '../../components/common/card/InfiniteScrollMovieCard.jsx';
 import useInfinityQueryList from '../../hook/useInfinityQueryList.js';
 import useIntersectionObserver from '../../hook/useIntersectionObserver.js';
 
 function Movies() {
-  const { data, fetchNextPage, hasNextPage } = useInfinityQueryList();
+  const { infiniteData, fetchNextPage, hasNextPage } = useInfinityQueryList();
   const ref = useIntersectionObserver(hasNextPage, fetchNextPage);
-  const allMovieData = useMemo(
-    () => data?.flatMap((v) => v.results.filter((a) => a.adult === false)) || [],
-    [data]
-  );
+  const [searchParam] = useSearchParams();
+  const { data } = useQuery({
+    queryKey: ['search', searchParam.get('query')],
+    queryFn: () => getSearchMovies(searchParam.get('query')),
+    enabled: searchParam.get('query') !== null,
+  });
 
   return (
     <div className="mt-28 grid grid-cols-4 items-center justify-between gap-4">
-      {allMovieData.map((v, i) => (
-        <InfiniteScrollMovieCard key={v.id} item={v} />
-      ))}
-      {hasNextPage && <div ref={ref} className="h-[8rem] bg-red-500" />}
+      {searchParam.get('query') ? (
+        data?.results?.length > 0 ? (
+          data.results.map((item) => <InfiniteScrollMovieCard key={item.id} item={item} />)
+        ) : (
+          <p>검색 결과가 없습니다.</p>
+        )
+      ) : (
+        infiniteData.map((item, i) => (
+          <InfiniteScrollMovieCard key={item.id + '-' + i} item={item} />
+        ))
+      )}
+      {hasNextPage && <div ref={ref} className="h-[7rem] " />}
     </div>
   );
 }
