d9b955205c341305c2464c6abf500907fca66cd1
Leekwanyong <dlrkdid12@gmail.com>
2025-03-14 04:03:53 +0900

    feat: 스토어 구현

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import darkModeReducer from './store/DarkModeSlice.js';
+import loginReducer from './store/LoginSlice.js';
 
 const store = configureStore({
   reducer: {
     darkMode: darkModeReducer,
+    OnLogin: loginReducer,
   },
 });
 
