acfbe24cfaf6c6e89687ac8672a90f82f31be010
Leekwanyong <dlrkdid12@gmail.com>
2025-03-14 04:10:50 +0900

    feat: 랜덤으로 보여주는 아이디를 state로 관리하여 기존 값을 계속 가지고 있게 함

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import ReactPlayer from 'react-player';
+import { useNavigate } from 'react-router';
 import Information from '../../components/common/Icon/Information.jsx';
 import Play from '../../components/common/Icon/Play.jsx';
 import { useGetMovieQuery } from '../../hook/useGetMovieQuery.js';
@@ -8,9 +9,15 @@ import useVideoFilter from '../../hook/useVideoFilter.js';
 const MOVIE_ID = ['950396', '1064213', '822119', '823219'];
 function HeroSection() {
   const random = MOVIE_ID[Math.floor(Math.random() * MOVIE_ID.length)];
-  const data = useGetMovieQuery(random);
+  const [randomId, setRandomId] = useState(random);
+  const data = useGetMovieQuery(randomId);
   const video = useVideoFilter(data);
   const [player, setPlayer] = useState(false);
+  const navigate = useNavigate();
+
+  const handlePlay = () => {
+    navigate(`/movieDetailModal/${randomId}`);
+  };
 
   return (
     <section className="w-full h-[50rem] relative " onMouseMove={() => setPlayer(true)}>
@@ -27,11 +34,17 @@ function HeroSection() {
       {video?.map((item) => (
         <div className="absolute top-[70%] left-[2%]" key={item?.id}>
           <div className="flex items-center justify-between gap-8 font-semibold">
-            <button className="bg-white text-black px-10 py-4 rounded-md flex items-center gap-2">
+            <button
+              className="bg-white text-black px-10 py-4 rounded-md flex items-center gap-2"
+              onClick={handlePlay}
+            >
               <Play />
               재생
             </button>
-            <button className="bg-transparent text-white px-10 py-4 rounded-md flex items-center gap-2">
+            <button
+              className="bg-transparent text-white px-10 py-4 rounded-md flex items-center gap-2"
+              onClick={handlePlay}
+            >
               <Information />
               상세 정보
             </button>
