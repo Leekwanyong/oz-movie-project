a2c446592582f15dc53c9ae37cd03cbe543ff326
Leekwanyong <dlrkdid12@gmail.com>
2025-03-14 21:23:31 +0900

    refactor: 인피니티 쿼리 캐시와, 가비지 시간을 주어서 불필요한 요청을 방지, 기존에 set을 reduce로 최적화하여 순회하는 최소로 하여 비용 감소, 특정조건에만 다음페이지를 불러오게 수정

--- src/hook/useInfinityQueryList.js
+++ src/hook/useInfinityQueryList.js
@@ -1,5 +1,5 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { getMovieList } from '../api/getMovieApi.js';
 
 function useInfinityQueryList() {
@@ -10,20 +10,40 @@ function useInfinityQueryList() {
     getNextPageParam: (lastPage) => {
       return lastPage.page < lastPage.total_pages ? lastPage.page + 1 : undefined;
     },
+    staleTime: 1000 * 60 * 5,
+    cacheTime: 1000 * 60 * 10,
   });
 
-  const allMovieData = useMemo(
-    () =>
-      infiniteQuery.data?.pages?.flatMap((v) => v.results.filter((a) => a.adult === false)) || [],
-    [infiniteQuery]
-  );
+  const allMovieData = useMemo(() => {
+    if (!infiniteQuery.data?.pages) return [];
+
+    const map = new Map();
+    infiniteQuery.data?.pages.reduce((_, page) => {
+      page.results.forEach(
+        (result) => {
+          if (!result.adult) {
+            map.set(result.id, result);
+          }
+          return map;
+        },
+        [map]
+      );
+    });
+    return Array.from(map.values());
+  }, [infiniteQuery.data?.pages]);
+
+  const loadMoreMovies = useCallback(() => {
+    if (infiniteQuery.hasNextPage && !infiniteQuery.isFetchingNextPage) {
+      infiniteQuery.fetchNextPage();
+    }
+  }, [infiniteQuery.hasNextPage, infiniteQuery.isFetchingNextPage]);
 
   return {
-    isLoading: infiniteQuery.isLoading ?? false,
+    isLoading: infiniteQuery.isLoading,
     infiniteData: allMovieData,
-    isFetchingNextPage: infiniteQuery.isFetchingNextPage ?? false,
-    fetchNextPage: infiniteQuery.fetchNextPage ?? (() => {}),
-    hasNextPage: infiniteQuery.hasNextPage ?? false,
+    isFetchingNextPage: infiniteQuery.isFetchingNextPage,
+    fetchNextPage: loadMoreMovies,
+    hasNextPage: infiniteQuery.hasNextPage,
   };
 }
 
