2df6f8b7109778f2f758eb4749630aa078c43cc1
Leekwanyong <dlrkdid12@gmail.com>
2025-03-14 22:58:27 +0900

    style: footer 반응형

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="flex align-center gap-16 mt-28 h-[20rem] ">
       <div className="text-grayDark">
-        <div className="flex gap-16">
+        <div className="lg:flex lg:flex-row md:flex md:flex-row sm:flex-col gap-16">
           <InfoList data={COMPANY_INFO} />
           <InfoList data={COMPANY} />
         </div>
--- src/layout/InfoList.jsx
+++ src/layout/InfoList.jsx
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react';
 function InfoList({ data = [] }) {
   const memoizedData = useMemo(() => data, [data]);
   return (
-    <ul>
+    <ul className="mb-4">
       {memoizedData?.map((item) => (
         <li key={item.label} className="mb-2">
           <p>
