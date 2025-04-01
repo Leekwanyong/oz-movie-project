8109704f494fa65a304167381c2e7d5c3b3b61f1
Leekwanyong <dlrkdid12@gmail.com>
2025-04-01 18:45:57 +0900

    chore: 테일윈드 config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('./src/styles/tailwind.colors.js');
 
 module.exports = {
   darkMode: 'class',
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors,
