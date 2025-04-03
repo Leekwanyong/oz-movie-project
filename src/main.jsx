8bd148da1119b0101d2be7ceb1eb6c041d6ac08a
Leekwanyong <dlrkdid12@gmail.com>
2025-04-03 16:00:33 +0900

    chore: 리액트 쿼리 데브툴 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
@@ -17,7 +16,6 @@ createRoot(document.getElementById('root')).render(
         <BrowserRouter>
           <App />
         </BrowserRouter>
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </Provider>
   </StrictMode>
