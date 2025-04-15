f3d5b92d12d2ebcc4f5c4544112a0881e9bd437e
Leekwanyong <dlrkdid12@gmail.com>
2025-04-15 19:11:03 +0900

    style: 스와이퍼 버튼 수정

--- src/constant/imageSize.js
+++ src/constant/imageSize.js
@@ -1,5 +1,5 @@
 export const IMAGE_SIZE = {
-  smail: 'https://image.tmdb.org/t/p/w500',
+  smail: 'https://image.tmdb.org/t/p/w200',
   medium: 'https://image.tmdb.org/t/p/w800',
-  large: 'https://image.tmdb.org/t/p/w1200'
-}
+  large: 'https://image.tmdb.org/t/p/w1200',
+};
