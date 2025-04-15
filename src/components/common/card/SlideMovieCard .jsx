a8a1ad286bfef2d936c6458ff030506d98e04c66
Leekwanyong <dlrkdid12@gmail.com>
2025-04-15 19:11:35 +0900

    style: SlideMoviecard ui css 수정

--- src/components/common/card/SlideMovieCard .jsx
+++ src/components/common/card/SlideMovieCard .jsx
@@ -9,13 +9,15 @@ function SlideMovieCard({ item }) {
     navigate(`/movieDetailModal/${item.id}`);
   };
 
-
   return (
-    <div onClick={handleOnNavigate}  className={`relative w-full rounded-md mt-4 max-w-[350px] h-[10rem] cursor-pointer transition-transform duration-300 `}>
-        <picture className='w-full aspect-video'>
-          <source srcSet={`${IMAGE_SIZE.smail}${item?.backdrop_path}`} type="image/webp" width={350} height={160}/>
+    <div
+      onClick={handleOnNavigate}
+      className={`relative w-full rounded-md mt-4 max-w-[350px] h-[10rem] group cursor-pointer transition-transform duration-300 overflow-hidden`}
+    >
+      <picture className="w-full h-full block">
+        <source srcSet={`${IMAGE_SIZE.smail}${item?.backdrop_path}`} type="image/webp" />
         <img
-          className="object-cover w-full h-auto rounded-md mr-4"
+          className="object-cover w-full h-full rounded-md"
           src={`${IMAGE_SIZE.smail}${item?.backdrop_path}`}
           alt={item?.title}
           width={350}
@@ -23,11 +25,11 @@ function SlideMovieCard({ item }) {
           loading="lazy"
           onError={handleImgError}
         />
-        </picture>
-        <div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
-          <h3 className="text-white text-lg font-bold">영화 제목: {item?.title}</h3>
-          <p className="text-white text-sm">평점 | {item?.vote_average}</p>
-        </div>
+      </picture>
+      <div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4 rounded-md">
+        <h3 className="text-white text-lg font-bold truncate">영화 제목: {item?.title}</h3>
+        <p className="text-white text-sm">평점 | {item?.vote_average}</p>
+      </div>
     </div>
   );
 }
