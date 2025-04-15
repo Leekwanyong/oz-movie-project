382af3ec6e8da90551647899da33e0c5acf50a8f
Leekwanyong <dlrkdid12@gmail.com>
2025-04-15 16:48:28 +0900

    refactor: axios 삭제 후 fetch로 변경

--- src/api/getMovieApi.js
+++ src/api/getMovieApi.js
@@ -1,43 +1,41 @@
-import axios from 'axios';
-
 const { VITE_TMDB_BASE_URL, VITE_TMDB_API_KEY } = import.meta.env;
 
+const fetchWithErrorHandling = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`API 호출 오류: ${response.status}`);
+  }
+
+  return response.json();
+};
+
 export const getPopularMovies = async (category) => {
-  const response = await axios.get(
+  return fetchWithErrorHandling(
     `${VITE_TMDB_BASE_URL}movie/${category}?api_key=${VITE_TMDB_API_KEY}&language=ko-KR`
   );
-
-  return response.data;
 };
 
 export const getMovieVideo = async (movieId) => {
-  const response = await axios.get(
+  return fetchWithErrorHandling(
     `${VITE_TMDB_BASE_URL}movie/${movieId}/videos?api_key=${VITE_TMDB_API_KEY}&language=en-US`
   );
-
-  return response.data;
 };
 
 export const getMovieDetail = async (movieId) => {
-  const response = await axios.get(
+  return fetchWithErrorHandling(
     `${VITE_TMDB_BASE_URL}movie/${movieId}?api_key=${VITE_TMDB_API_KEY}&language=ko-KR`
   );
-
-  return response.data;
 };
 
 export const getMovieList = async (pageParam = 1) => {
-  const response = await axios.get(
+  return fetchWithErrorHandling(
     `${VITE_TMDB_BASE_URL}discover/movie?api_key=${VITE_TMDB_API_KEY}&include_adult=false&language=ko-KR&page=${pageParam}`
   );
-
-  return response.data;
 };
 
 export const getSearchMovies = async (value) => {
-  const response = await axios.get(
+  return fetchWithErrorHandling(
     `${VITE_TMDB_BASE_URL}search/movie?query=${value}&api_key=${VITE_TMDB_API_KEY}&include_adult=false&language=ko-KR`
   );
-
-  return response.data;
 };
